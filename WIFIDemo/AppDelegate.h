75c007e6d2267244e1ef9042ab0318254e62645b
948769029 <apple@appledeMacBook-Pro.local>
2016-10-19 10:22:00 +0800

    Initial Commit

---
+++ WIFIDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WIFIDemo
+//
+//  Created by apple on 2016/10/19.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
